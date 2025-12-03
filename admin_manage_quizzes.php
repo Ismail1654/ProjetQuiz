9483ce67dde66840c478fa575b77547cc150269a
ismail <ismailyous@yahoo.com>
2025-12-03 10:42:59 +0100

    index.php

--- admin_manage_quizzes.php
+++ admin_manage_quizzes.php
@@ -1,7 +1,6 @@
 <?php
-// admin_manage_quizzes.php - Redirige vers la page de gestion centrale list_quizzes.php
+
 require_once 'functions.php';
 
-// Redirection immédiate vers la page list_quizzes, car elle est centralisée.
-redirect('list_quizzes.php');
+redirect('list_quizzes.php'); 
 ?>
