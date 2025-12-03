a23607da6d40ccdf252ab142716a303ed7d8e60f
vidc01 <victor.dct01@gmail.com>
2025-12-03 10:24:59 +0100

    premier commit

---
+++ admin_manage_quizzes.php
@@ -0,0 +1,7 @@
+<?php
+// admin_manage_quizzes.php - Redirige vers la page de gestion centrale list_quizzes.php
+require_once 'functions.php';
+
+// Redirection immédiate vers la page list_quizzes, car elle est centralisée.
+redirect('list_quizzes.php');
+?>
